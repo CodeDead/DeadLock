e3a849ca0927e022aa0e33982c8589dc856c156b
CodeDead <admin@codedead.com>
2017-03-11 01:11:02 +0100

    * Added toolbar building blocks

--- DeadLock-WPF/Properties/Resources.Designer.cs
+++ DeadLock-WPF/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace DeadLock.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap deadlock {
+            get {
+                object obj = ResourceManager.GetObject("deadlock", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
--- DeadLock-WPF/Windows/MainWindow.xaml.cs
+++ DeadLock-WPF/Windows/MainWindow.xaml.cs
@@ -10,10 +10,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DeadLock_WPF
+namespace DeadLock.Windows
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
